05d0b7c4da1c6033985347fb7c124fad5255e721
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2023-10-08 15:08:26 -0700

    uncomment and follow

---
+++ File_Coverter/File_Coverted.py
@@ -0,0 +1,13 @@
+#To install PyInstaller, you can use Python's package manager, pip. Here are the steps to install PyInstaller:
+
+#Open your command prompt or terminal.
+#Make sure you have Python installed on your system. You can check if Python is installed by running:
+
+#python --version
+
+#Once you have Python installed, you can install PyInstaller using pip. Run the following command:
+#pip install pyinstaller
+
+#After the installation is complete, you can verify that PyInstaller was installed successfully by running:
+# pyintaller --version
+
