bac8f26688454b273e6622fd3d5095e65e310ebe
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2023-10-08 15:44:50 -0700

    Ad_Blocker

---
+++ Ad_Blocker/Ad_Blocker.py
@@ -0,0 +1,29 @@
+#import datetime
+
+#end_time = datetime.datetime(Year,Month,Day)
+#site_bloack = ["Name of webiste", "Name2"]
+#local_path = ("Path of website")
+#redirect = "Number"
+
+#while True:
+    #if datetime.datetime.now() < end_time:
+        #print("Start Blocking")
+        #with open(local_path , "r+") as host_file:
+            #content = host_file.read()
+            #for web in site_block:
+                #if webiste not in content:
+                    #host_filr.write(redirect + " " + website + "\n")
+                #else: 
+                    #
+    #else:
+        #with open(local_path , "r+") as host_file:
+            #content = host_file.readlines()
+            #host_filr.seek(0)
+            #for lines in content:
+                #if not any(website in lines for website in site_block):
+                #host_file.write(lines)
+            
+            #host_fail.truncate()
+        #time.sleep(5)
+
+        
